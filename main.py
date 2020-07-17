524e726118141be6b549ceb9250ed27575f89379
pjkpjk900 <pk58@buffalo.edu>
2020-07-17 11:01:13 -0400

    Return data as a dictionary

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ with open('5minutes.html') as f:
     soup = BeautifulSoup(f, 'html.parser')
 
 a_elements  = soup.find_all('a', href = True)
+text_to_url = {}
 
 for a in a_elements:
-    print(a['href'])
+    text_to_url[a.getText()] = a['href']
+
+print(text_to_url)
